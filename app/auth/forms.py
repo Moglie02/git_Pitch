e6d769b7eb929a037c0186e49e129ca2ba5f2f82
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-11 11:46:55 +0300

    https://github.com/Moglie02/Pitch-world.git

---
+++ app/auth/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import ValidationError, StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import InputRequired,Email,EqualTo
+from ..models import User
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[InputRequired()])
+    password = PasswordField('Password',validators=[InputRequired()])
+    remember = BooleanField('Remember Me!')
+    submit = SubmitField('Login')
+
+class RegForm(FlaskForm):
+    email = StringField('Your Email Address', validators=[InputRequired(),Email()])
+    username = StringField('Enter Your Username', validators=[InputRequired()])
+    password = PasswordField('Password',validators = [InputRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError("The Email has already been taken!")
+    
+    def validate_username(self, data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError("The username has already been taken")
