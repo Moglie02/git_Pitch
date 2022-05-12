d516acc60d83551a6bdda44a3b9863a0a274424a
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-05-12 18:41:51 +0300

    added env

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
 import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
 class Config:
- 
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS=True
-    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SECRET_KEY=os.getenv('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 
 class ProdConfig(Config):
-      SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+      SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
 
 class DevConfig(Config):
